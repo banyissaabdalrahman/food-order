8bef21e10e507a29047b66f0d9901fa334273c5a
Abd Alrahman Bany Issa <81553723+banyissaabdalrahman@users.noreply.github.com>
2023-08-09 16:10:11 +0300

    Adding a Form

--- src/components/Meals/MealItem/AvailableMeals.js
+++ src/components/Meals/MealItem/AvailableMeals.js
@@ -1,7 +1,6 @@
-import React from "react";
-import classes from "./AvailableMeals.module.css";
 import Card from "../../UI/Card";
 import MealItem from "./MealItem";
+import classes from "./AvailableMeals.module.css";
 
 const DUMMY_MEALS = [
   {
@@ -33,11 +32,14 @@ const DUMMY_MEALS = [
 const AvailableMeals = () => {
   const mealsList = DUMMY_MEALS.map((meal) => (
     <MealItem
+      id={meal.id}
+      key={meal.id}
       name={meal.name}
       description={meal.description}
       price={meal.price}
     />
   ));
+
   return (
     <section className={classes.meals}>
       <Card>
